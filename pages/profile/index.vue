d2e7156083845e0247dca0001206b97d101c205d
liuwei2015 <liuwei2015@daojia-inc.com>
2021-07-03 20:56:44 +0800

    feat: 更新

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,54 +1,74 @@
 <template>
   <div class="profile-page">
-
     <div class="user-info">
       <div class="container">
         <div class="row">
-
           <div class="col-xs-12 col-md-10 offset-md-1">
             <img src="http://i.imgur.com/Qr71crq.jpg" class="user-img" />
             <h4>Eric Simons</h4>
-            <p>
-              Cofounder @GoThinkster, lived in Aol's HQ for a few months, kinda looks like Peeta from the Hunger Games
-            </p>
+            <p>Cofounder @GoThinkster, lived in Aol's HQ for a few months, kinda looks like Peeta from the Hunger Games</p>
             <button class="btn btn-sm btn-outline-secondary action-btn">
               <i class="ion-plus-round"></i>
               &nbsp;
               Follow Eric Simons
             </button>
           </div>
-
         </div>
       </div>
     </div>
 
     <div class="container">
       <div class="row">
-
         <div class="col-xs-12 col-md-10 offset-md-1">
           <div class="articles-toggle">
             <ul class="nav nav-pills outline-active">
               <li class="nav-item">
-                <a class="nav-link active" href="">My Articles</a>
+                <nuxt-link
+                  class="nav-link"
+                  :class="{
+                    active: tab === 'my_article',
+                  }"
+                  exact
+                  :to="{
+                    name: 'home',
+                    query: {
+                      tab: 'your_feed',
+                    },
+                  }"
+                >My Articles</nuxt-link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="">Favorited Articles</a>
+                <nuxt-link
+                  class="nav-link"
+                  :class="{
+                    active: tab === 'my_favorited',
+                  }"
+                  exact
+                  :to="{
+                    name: 'home',
+                    query: {
+                      tab: 'your_feed',
+                    },
+                  }"
+                >Favorited Articles</nuxt-link>
               </li>
             </ul>
           </div>
 
           <div class="article-preview">
             <div class="article-meta">
-              <a href=""><img src="http://i.imgur.com/Qr71crq.jpg" /></a>
+              <a href>
+                <img src="http://i.imgur.com/Qr71crq.jpg" />
+              </a>
               <div class="info">
-                <a href="" class="author">Eric Simons</a>
+                <a href class="author">Eric Simons</a>
                 <span class="date">January 20th</span>
               </div>
               <button class="btn btn-outline-primary btn-sm pull-xs-right">
                 <i class="ion-heart"></i> 29
               </button>
             </div>
-            <a href="" class="preview-link">
+            <a href class="preview-link">
               <h1>How to build webapps that scale</h1>
               <p>This is the description for the post.</p>
               <span>Read more...</span>
@@ -57,16 +77,18 @@
 
           <div class="article-preview">
             <div class="article-meta">
-              <a href=""><img src="http://i.imgur.com/N4VcUeJ.jpg" /></a>
+              <a href>
+                <img src="http://i.imgur.com/N4VcUeJ.jpg" />
+              </a>
               <div class="info">
-                <a href="" class="author">Albert Pai</a>
+                <a href class="author">Albert Pai</a>
                 <span class="date">January 20th</span>
               </div>
               <button class="btn btn-outline-primary btn-sm pull-xs-right">
                 <i class="ion-heart"></i> 32
               </button>
             </div>
-            <a href="" class="preview-link">
+            <a href class="preview-link">
               <h1>The song you won't ever stop singing. No matter how hard you try.</h1>
               <p>This is the description for the post.</p>
               <span>Read more...</span>
@@ -76,23 +98,24 @@
               </ul>
             </a>
           </div>
-
-
         </div>
-
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
-  middleware: 'authenticated',
-  name: 'UserProfile'
-}
+  middleware: "authenticated",
+  name: "UserProfile",
+
+  data: function () {
+    return {
+      tab: "my_article",
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
